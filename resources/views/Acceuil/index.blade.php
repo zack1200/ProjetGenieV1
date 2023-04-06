e2b2be05f50a23b306277717a7c4336bb2924851
zack1200 <102002192+zack1200@users.noreply.github.com>
2023-04-06 16:37:59 -0400

    d

---
+++ resources/views/Acceuil/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  
+    <link rel="stylesheet" href="{{asset('css/style.css') }}">
+    <title>Document</title>
+</head>
+<body>
+<div class="container py-5 " >
+                <div class="row align-items-center text-center">
+                    <div class="col-xl-12 col-md-8 col-sm-12">
+                        <h1>titre</h1>
+                    </div>
+                </div>
+            </div> 
+    
+        <div class="container py-5">
+            <div class="row align-items-center text-center bgg py-5 ">
+            <div class="col-xl-6 col-md-12 col-sm-12  "> 
+                        <img src="https://wallpaperaccess.com/full/3423989.jpg" alt="<!--description-->"class="img-fluid px-5" >
+                </div>
+                <div class="col-xl-6 col-md-12 col-sm-12 "> 
+                        <h1>Technique Informatique     </h1>
+                        <h2>Session hiver 2023</h2>
+                        
+                            <h3>colors</h3>
+                        <h3>tailles</h3>
+                        
+                        
+                        
+                       
+                        <a href= "<!--adresse d un site-->" class="btn btn-primary" target="_blank" rel="noopener noreferrer">..........</a>       
+                </div>
+                
+            </div>
+        </div>
+        
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+</body>
+</html>
