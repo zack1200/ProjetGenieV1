8db58ab75770b991eb22495cc576c667aad82464
zack1200 <102002192+zack1200@users.noreply.github.com>
2023-04-08 18:28:20 -0400

    Acceuil interface

--- resources/views/Acceuil/index.blade.php
+++ resources/views/Acceuil/index.blade.php
@@ -14,14 +14,15 @@
   <a href="#" class="logo"><img src="{{asset('img/logo.png') }}" alt="Logo"></a>
   <nav>
     
-    <a href="#" class="cta-btn">Bouton</a>
+    <a href="#" class="cta-btn">Se connecter</a>
+    
   </nav>
 </header>
 
 
 <div class="container  " >
                 <div class="row align-items-center text-center">
-                    <div class="col-xl-12 col-md-8 col-sm-12">
+                    <div class="col-xl-12 col-md-12 col-sm-12 ">
                         <h1>Merch Session Hiver 2023</h1>
                     </div>
                 </div>
@@ -29,65 +30,51 @@
     
         <div class="container ">
             <div class="row align-items-center text-center  py-5 ">
-            <div class="col-xl-6 col-md-12 col-sm-12  ">
+            <div class="col-xl-6 col-md-6 col-sm-6  ">
                 <div class="row align-items-center text-center bggg py-5 ">
-                <div class="offset-xl-1 col-xl-4 col-md-12 col-sm-12 bg py-5 px-5 ">
-                <!-- 
-            <div class="slider">
-                        <div class="slider-container">
-                        <img src="https://th.bing.com/th/id/OIP.EF_rH_CGlInjFiT_D71OjAAAAA?pid=ImgDet&rs=1" alt="Image 1">
-                        <img src="https://th.bing.com/th/id/OIP.cFLqD9dhoCoQFy4-QfS3GAHaEK?pid=ImgDet&w=770&h=433&rs=1" alt="Image 2">
-                        <img src="chemin/vers/image3.jpg" alt="Image 3">
-                        </div>
-                                <div class="slider-controls">
-                                <button class="slider-btn prev-btn">&#8249;</button>
-                                <button class="slider-btn next-btn">&#8250;</button>
-                                </div>
-
-             </div>
--->
-<div class="slider">
-  <div class="slider-container">
-    <img src="https://th.bing.com/th/id/OIP.EF_rH_CGlInjFiT_D71OjAAAAA?pid=ImgDet&rs=1" alt="Image 1">
-    <img src="https://th.bing.com/th/id/OIP.cFLqD9dhoCoQFy4-QfS3GAHaEK?pid=ImgDet&w=770&h=433&rs=1" alt="Image 2">
-    
-  </div>
-  <button class="prevBtn">&lt;</button>
-  <button class="nextBtn">&gt;</button>
-</div>
-
-
-             </div>
+                <div class="offset-xl-1 col-xl-4 col-md-4 col-sm-4 bgg py-5 px-5 ">
+                
+                <div class="slider">
+                <div class="slider-container">
+                    <img src="https://wallpapercave.com/wp/wp3185345.jpg" alt="Image 1">
+                    <img src="https://wallpapercave.com/wp/wp3185345.jpg" alt="Image 2">
+                    
+                </div>
+                <button class="prevBtn">&lt;</button>
+                <button class="nextBtn">&gt;</button>
+                </div>
+            </div>
              </div>
              </div>
              
 
-                <div class="col-xl-6 col-md-12 col-sm-12 bgg py-5 px-5"> 
+                <div class="col-xl-6 col-md-6 col-sm-6 bgg py-5 px-5"> 
                  
                          <h1>Formulaire de commande de T-shirt</h1>
                          <form id="shirt-form">
-                           <label>Couleur:</label>
-                                                    <div class="color-options">
-                                                    <div class="color-option red" id="color-red"></div>
-                                                    <div class="color-option green" id="color-green"></div>
-                                                    <div class="color-option blue" id="color-blue"></div>
-                                                    </div>
+                                    <label>Couleur:</label>
+                                <div class="color-options">
+                                <div class="color-option red" id="color-red"></div>
+                                <div class="color-option green" id="color-green"></div>
+                                <div class="color-option blue" id="color-blue"></div>
+                                </div>
                                                     
-                                                    <div class="size-options">
-                                                    <label>Taille:</label>
-                                                    <div class="size-option">S</div>
-                                                    <div class="size-option">M</div>
-                                                    <div class="size-option">L</div>
-                                                    <div class="size-option">XL</div>
-                                                    </div>
-
-                                                    <button type="submit">Commander</button>
+                                <div class="size-options">
+                                    <label>Taille:</label>
+                                    <div class="size-option">S</div>
+                                    <div class="size-option">M</div>
+                                    <div class="size-option">L</div>
+                                    <div class="size-option">XL</div>
+                                 </div>
+                                <button type="submit" class="cta-btn">Reserver</button>
                          </form>
 
                 </div>
                 
             </div>
         </div>
+
+        
 
         <script src="js/Acceuil.js" defer></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
