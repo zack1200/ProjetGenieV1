ade025b656b0902a92ca97529fadbd3d9c128d5b
zack1200 <102002192+zack1200@users.noreply.github.com>
2023-04-16 15:16:20 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\UsersController;
 */
 
 Route::get('/', function () {
-    return view('Acceuil/index');
+    return "re";//view('Acceuil/index');
 });
 Route::get('/login', function () {
     return view('Connexion/login');
