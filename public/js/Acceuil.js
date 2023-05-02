d1d4ff6e90fe359bcdba8c6de9595c9b75e79c2b
zack1200 <102002192+zack1200@users.noreply.github.com>
2023-05-02 11:17:15 -0400

    05:02

--- public/js/Acceuil.js
+++ public/js/Acceuil.js
@@ -1,98 +1,25 @@
-const sliderContainer = document.querySelector('.slider-container');
-const prevBtn = document.querySelector('.prevBtn');
-const nextBtn = document.querySelector('.nextBtn');
-const slides = sliderContainer.querySelectorAll('img');
-const totalSlides = slides.length;
-let slideIndex = 0;
-let autoSlideInterval;
+// Récupérer tous les boutons de couleur
+let colorButtons = document.querySelectorAll('.color-sample-btn');
 
-// Afficher l'image actuelle
-function showSlide(index) {
-  sliderContainer.style.transform = `translateX(-${index * 25}%)`;
-}
+// Ajouter un gestionnaire d'événements de clic à chaque bouton de couleur
+colorButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    // Supprimer la classe active de tous les boutons de couleur
+    colorButtons.forEach(btn => btn.classList.remove('active'));
 
-// Fonction pour faire défiler l'image suivante
-function nextSlide() {
-  slideIndex++;
-  if (slideIndex === totalSlides) {
-    slideIndex = 0;
-  }
-  showSlide(slideIndex);
-}
-
-// Fonction pour faire défiler l'image précédente
-function prevSlide() {
-  slideIndex--;
-  if (slideIndex < 0) {
-    slideIndex = totalSlides - 1;
-  }
-  showSlide(slideIndex);
-}
-
-// Écouter les clics sur les boutons
-nextBtn.addEventListener('click', () => {
-  nextSlide();
-  clearInterval(autoSlideInterval);
+    // Ajouter la classe active au bouton cliqué
+    this.classList.add('active');
+  });
+});
+$(document).ready(function() {
+  $('.taille-sample-btn').click(function() {
+    // Désactive toutes les tailles
+    $('.taille-sample-btn').removeClass('active');
+    // Active la taille sélectionnée
+    $(this).addClass('active');
+    // Met à jour la valeur sélectionnée
+    $('#selected-taille').val($(this).data('value'));
+  });
 });
 
-prevBtn.addEventListener('click', () => {
-  prevSlide();
-  clearInterval(autoSlideInterval);
-});
-
-// Faire défiler automatiquement toutes les 5 secondes
-autoSlideInterval = setInterval(() => {
-  nextSlide();
-}, 5000);
-
- // Récupérer les éléments de l'interface utilisateur
- var colorOptions = document.querySelectorAll('.color-option');
- var sizeOptions = document.querySelectorAll('.size-option');
- var form = document.getElementById('shirt-form');
-
- // Ajouter des écouteurs d'événements aux éléments de l'interface utilisateur
- colorOptions.forEach(function(colorOption) {
-   colorOption.addEventListener('click', function(event) {
-     colorOptions.forEach(function(colorOption) {
-       colorOption.classList.remove('selected');
-     });
-     event.target.classList.add('selected');
-   });
- });
-
- sizeOptions.forEach(function(sizeOption) {
-   sizeOption.addEventListener('click', function(event) {
-     sizeOptions.forEach(function(sizeOption) {
-       sizeOption.classList.remove('selected');
-     });
-     event.target.classList.add('selected');
-   });
- });
-
- form.addEventListener('submit', function(event) {
-   event.preventDefault();
-   var color = document.querySelector('.color-option.selected').id;
-   var size = document.querySelector('.size-option.selected').textContent;
-   console.log('Couleur sélectionnée:', color);
-   console.log('Taille sélectionnée:', size);
- });
-
- // sélectionner le bouton du mode nuit
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-
-// écouter l'événement click sur le bouton
-darkModeToggle.addEventListener('click', () => {
-  // ajouter ou supprimer la classe 'dark-mode' sur le body
-  document.body.classList.toggle('dark-mode');
-});
-
-
-
-
-
-
-
-
-  
-  
 
--- resources/views/Acceuil/index.blade.php
+++ resources/views/Acceuil/index.blade.php
@@ -10,12 +10,17 @@
     <title>Document</title>
 </head>
 <body>
-<header>
-  <a href="/" class="logo "><img src="{{asset('img/logo.png') }}" alt="Logo"></a>
-  
 
- <a href="/login" class="cta-btn">Se connecter</a>
-</header>
+<nav class="navbar">
+  <div class="navbar-logo">
+    <a href="#"><img src="{{asset('img/logo.png') }}" alt="Logo"></a>
+  </div>
+  <ul class="navbar-options">
+    <li><a href="#"><img src="{{asset('img/icon/basket.png') }}" alt="" width="30px" height="30px"></a></li>
+    <li><a href="/login">Connexion</a></li>
+  </ul>
+</nav>
+ 
 
 
 
@@ -26,8 +31,7 @@
                     <div class="col-xl-12 col-md-12 col-sm-12 tit ">
                     @foreach ($compaigns as $compaign)
                       <h1>{{$compaign->nom}} </h1>
-                    @endforeach
-                        
+                    @endforeach                        
                     </div>
                 </div>
             </div>
@@ -39,33 +43,55 @@
              @if (isset($compaign->items) && count($compaign->items))
             @foreach ($compaign->items as $itemcompaign) 
             
-        <div class="container col-xl-12">
-      <div class="row align-items-center text-center  py-3">
+            <div class="container col-xl-12">
+    <div class="row align-items-center text-center py-3">
         <div class="col-xl-4 bggg py-5">
-          <div class=" offset-xl-2 col-xl-4 col-md-4 col-sm-4 bgg py-5 px-5 ">
-          <img src="{{ asset('img/model/' . $itemcompaign->mookup) }}" alt="Logo" width="100px" height="100px">                              
-          </div>
-          <div class=" col-xl-12 col-md-4 col-sm-4 Desc py-4 ">
-          <h4>{{$itemcompaign->nom}}</h4>
-          <h5>Couleurs disponibles</h5>
+            <div class="offset-2 col-xl-10 col-md-10 col-sm-10 bgg py-5 px-5 ">
+                <img src="{{ asset('img/model/' . $itemcompaign->mookup) }}" alt="Logo" width="100px" height="100px">
+            </div>
+            <div class="col-xl-12 col-md-12 col-sm-12 Desc py-4">
+                <h4>{{$itemcompaign->nom}}</h4>
+                <!-- <form action="..." method="post"> -->
+                    @csrf
                     <div class="container">
-                     @foreach ($itemcompaign->color as $itemColor) 
-                    <div class="color-sample1 " style="background-color: {{$itemColor->CodeCouleur}}"></div>
-                      @endforeach
+                        <h5>Choisir une couleur</h5>
+                        <div class="">
+                            @foreach ($itemcompaign->color as $itemColor)
+                            <button type="button" class="color-sample-btn" style="background-color: {{$itemColor->CodeCouleur}};" data-value="{{$itemColor->CodeCouleur}}"> 
+                            </button>                           
+                            @endforeach
+                        </div>
+                        <input type="hidden" name="couleur" id="selected-color" value="">
+                    </div>
+
+                    <div class="container">
+                        <h5>Choisir une taille</h5>
+                        <div class=" ">
+                            @foreach ($itemcompaign->taille as $itemtaille)
+                            <button type="button" class="taille-sample-btn" data-value="{{$itemtaille->nomtaille}}">{{$itemtaille->nomtaille}}</button>
+                            @endforeach
+                        </div>
+                        <input type="hidden" name="taille" id="selected-taille" value="">
+                    </div>
+
+                    <div class="container">
+                        <h5>Quantité</h5>
+                        <div class="quantite">
                         
+                            <input type="number" name="quantite" value="1" min="1" max="3">
+                        
+                        </div>
                     </div>
-                    
+
                     <div class="container">
-                    <h5>Échantillons de taille</h5>
-                    
-                    @foreach ($itemcompaign->taille as $itemtaille)
-                    {{$itemtaille->nomtaille}}
-                    @endforeach
-                    
+                        <button type="submit">Ajouter au panier</button>
                     </div>
-          </div>
-          
+                </form>
+            </div>
         </div>
+    </div>
+</div>
+
         @endforeach
         @else
         <h1>Une campagne sera bientot publier</h1>
